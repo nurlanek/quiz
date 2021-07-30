bf9dedbe911a85381f2e5a654a0e0247fa33dd2e
nurlanek <nurlanbek1@gmail.com>
2021-07-30 21:17:57 +0200

    jetsteam --team install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{$header}} - {{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -28,20 +28,15 @@
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                              {{ $header }}
-                        </h2>
+                        {{ $header }}
                     </div>
                 </header>
             @endif
 
             <!-- Page Content -->
-            <div class="py-6">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                      {{ $slot }}
-                </div>
-            </div>
-
+            <main>
+                {{ $slot }}
+            </main>
         </div>
 
         @stack('modals')

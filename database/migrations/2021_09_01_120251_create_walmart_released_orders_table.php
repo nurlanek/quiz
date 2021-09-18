40df8d5e1bed56bf5cbe98a679933e1fff2acc28
nurlanek <nurlanbek1@gmail.com>
2021-09-18 14:00:17 +0200

    new migrate

--- database/migrations/2021_09_01_120251_create_walmart_released_orders_table.php
+++ database/migrations/2021_09_01_120251_create_walmart_released_orders_table.php
@@ -15,15 +15,61 @@ class CreateWalmartReleasedOrdersTable extends Migration
 
       {
             Schema::create('walmart_released_orders', function (Blueprint $table) {
-                $table->id();
-                $table->string('customer_email_id')->nullable();
-                $table->string('customer_order_id')->default('');
-                $table->string('order_date')->default('');
-                $table->longText('order_lines')->nullable();
-                $table->string('order_type')->default('');
-                $table->string('original_customer_order_id')->nullable();
-                $table->string('purchase_order_id')->default('');
-                $table->longText('shipping_info')->nullable();
+                  $table->id();
+                  $table->string('purchaseOrderId')->nullable();
+                  $table->string('customerOrderId')->nullable();
+                  $table->string('customerEmailId')->nullable();
+                  $table->string('orderType')->nullable();
+                  $table->string('originalCustomerOrderID')->nullable();
+                  $table->string('orderDate')->nullable();
+                  $table->string('shippingInfo')->nullable();
+                  $table->string('phone')->nullable();
+                  $table->string('estimatedDeliveryDate')->nullable();
+                  $table->string('estimatedShipDate')->nullable();
+                  $table->string('methodCode')->nullable();
+                  $table->string('postalAddress')->nullable();
+                  $table->string('name')->nullable();
+                  $table->string('address1')->nullable();
+                  $table->string('address2')->nullable();
+                  $table->string('city')->nullable();
+                  $table->string('state')->nullable();
+                  $table->string('postalCode')->nullable();
+                  $table->string('country')->nullable();
+                  $table->string('addressType')->nullable();
+                  $table->string('orderLines')->nullable();
+                  $table->string('orderLine')->nullable();
+                  $table->string('lineNumber')->nullable();
+                  $table->string('item')->nullable();
+                  $table->string('productName')->nullable();
+                  $table->string('sku')->nullable();
+                  $table->string('charges')->nullable();
+                  $table->string('charge')->nullable();
+                  $table->string('chargeType')->nullable();
+                  $table->string('chargeName')->nullable();
+                  $table->string('chargeAmount')->nullable();
+                  $table->string('currency')->nullable();
+                  $table->string('amount')->nullable();
+                  $table->string('tax')->nullable();
+                  $table->string('taxName')->nullable();
+                  $table->string('taxAmount')->nullable();
+                  $table->string('currency')->nullable();
+                  $table->string('amount')->nullable();
+                  $table->string('orderLineQuantity')->nullable();
+                  $table->string('unitOfMeasurement')->nullable();
+                  $table->string('amount')->nullable();
+                  $table->string('statusDate')->nullable();
+                  $table->string('orderLineStatuses')->nullable();
+                  $table->string('orderLineStatus')->nullable();
+                  $table->string('status')->nullable();
+                  $table->string('statusQuantity')->nullable();
+                  $table->string('unitOfMeasurement')->nullable();
+                  $table->string('amount')->nullable();
+                  $table->string('fulfillment')->nullable();
+                  $table->string('fulfillmentOption')->nullable();
+                  $table->string('shipMethod')->nullable();
+                  $table->string('pickUpDateTime')->nullable();
+
+
             });
         }
 
